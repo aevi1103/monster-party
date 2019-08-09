c8933c167d524645695de67af8618faf14e8c69b
aevi <aevirontos@gmail.com>
2019-08-08 22:04:10 -0400

    Updates

---
+++ precache-manifest.995cae17a0494ec9ef73331dbd2f6f25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35fd089d5b10adde672817bc1f3e0b33",
+    "url": "/monster-party/index.html"
+  },
+  {
+    "revision": "bf17caabbc0f353a42ac",
+    "url": "/monster-party/static/css/main.62387c4e.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monster-party/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "bf17caabbc0f353a42ac",
+    "url": "/monster-party/static/js/main.c957192d.chunk.js"
+  },
+  {
+    "revision": "6e9c8537b50f89de9af7",
+    "url": "/monster-party/static/js/runtime~main.2f31599d.js"
+  }
+]);
